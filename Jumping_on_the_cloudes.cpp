883fe23bb1dd47c8b43cefee1b09f291aca4b3e7
Rupam Ghosh <rupamghosh90693@gmail.com>
2020-06-06 12:53:47 +0530

    well done

---
+++ Jumping_on_the_cloudes.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int n,temp,i=0,jump=0;
+    vector<int>v;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>temp;
+        v.push_back(temp);
+
+    }
+    i=0;jump=0;
+    while(i<n-1)
+    {
+             if(v[i+2]==1)
+             {
+                i++;
+                jump++;
+             }
+             else
+             {
+                   i=i+2;
+                   jump++;
+             }
+     }
+    cout<<jump;
+}
